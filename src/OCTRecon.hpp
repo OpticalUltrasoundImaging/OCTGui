24fe58421e73fe939c0a31dd3c9abf8002e54fc7
tnie <tnie@tuta.io>
2024-12-20 02:42:34 -0600

    parallel recon

--- src/OCTRecon.hpp
+++ src/OCTRecon.hpp
@@ -90,56 +90,55 @@ reconBscan(const Calibration<T> &calib, const std::span<const uint16_t> fringe,
   cv::Mat_<uint8_t> mat(nLines, imageDepth);
 
   const auto win = getHamming<T>(ALineSize);
-
   const auto contrast = conversionParams.contrast;
   const auto brightness = conversionParams.brightness;
 
-  std::vector<T> alineBuf(ALineSize);
-  std::vector<T> linearKFringe(ALineSize);
-
-  auto &fft = fftw::EngineR2C1D<T>::get(ALineSize);
+  const auto &fft = fftw::EngineR2C1D<T>::get(ALineSize);
 
   const cv::Range range(0, nLines);
-  // cv::parallel_for_(range, [&](cv::Range range) {
-  fftw::R2CBuffer<T> fftBuf(ALineSize);
+  cv::parallel_for_(range, [&](cv::Range range) {
+    fftw::R2CBuffer<T> fftBuf(ALineSize);
+    std::vector<T> alineBuf(ALineSize);
+    std::vector<T> linearKFringe(ALineSize);
 
-  for (int j = range.start; j < range.end; ++j) {
-    const auto offset = j * ALineSize;
+    for (int j = range.start; j < range.end; ++j) {
+      const auto offset = j * ALineSize;
 
-    // 1. Subtract background
-    for (int i = 0; i < ALineSize; ++i) {
-      alineBuf[i] = fringe[offset + i] - calib.background[i];
+      // 1. Subtract background
+      for (int i = 0; i < ALineSize; ++i) {
+        alineBuf[i] = fringe[offset + i] - calib.background[i];
+      }
+
+      // 2. Interpolate phase calibration data
+      for (int i = 0; i < ALineSize - 1; ++i) {
+        const auto idx = calib.phaseCalib[i].idx;
+        const auto unit = calib.phaseCalib[idx];
+        const auto l_coeff = unit.l_coeff;
+        const auto r_coeff = unit.r_coeff;
+        linearKFringe[i] =
+            alineBuf[idx] * l_coeff + alineBuf[idx + 1] * r_coeff;
+      }
+
+      // 3. FFT
+      // Window
+      for (int i = 0; i < ALineSize; ++i) {
+        fftBuf.in[i] = win[i] * linearKFringe[i];
+      }
+      fft.forward(fftBuf.in, fftBuf.out);
+
+      // 4. Copy result into image
+      uint8_t *outptr = mat.ptr(j);
+      for (size_t i = 0; i < imageDepth; ++i) {
+        const auto ro = fftBuf.out[i][0];
+        const auto io = fftBuf.out[i][1];
+
+        T val = ro * ro + io * io;
+        val = contrast * (10 * log10(val) + brightness);
+        val = val < 0 ? 0 : val > 255 ? 255 : val;
+        outptr[i] = static_cast<uint8_t>(val);
+      }
     }
-
-    // 2. Interpolate phase calibration data
-    for (int i = 0; i < ALineSize - 1; ++i) {
-      const auto idx = calib.phaseCalib[i].idx;
-      const auto unit = calib.phaseCalib[idx];
-      const auto l_coeff = unit.l_coeff;
-      const auto r_coeff = unit.r_coeff;
-      linearKFringe[i] = alineBuf[idx] * l_coeff + alineBuf[idx + 1] * r_coeff;
-    }
-
-    // 3. FFT
-    // Window
-    for (int i = 0; i < ALineSize; ++i) {
-      fftBuf.in[i] = win[i] * linearKFringe[i];
-    }
-    fft.forward(fftBuf.in, fftBuf.out);
-
-    // 4. Copy result into image
-    uint8_t *outptr = mat.ptr(j);
-    for (size_t i = 0; i < imageDepth; ++i) {
-      const auto ro = fftBuf.out[i][0];
-      const auto io = fftBuf.out[i][1];
-
-      T val = ro * ro + io * io;
-      val = contrast * (10 * log10(val) + brightness);
-      val = val < 0 ? 0 : val > 255 ? 255 : val;
-      outptr[i] = static_cast<uint8_t>(val);
-    }
-  }
-  // });
+  });
 
   mat = mat.t();
 
